7802f562a3210dff36bbfb3b5c9dab7ed8443737
islom1212 <islomaktamov488>
2025-07-20 23:22:36 +0500

    add new codes

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        xs: "375px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
